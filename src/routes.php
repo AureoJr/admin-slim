351383a158a0a3b7034a3b2453361250ba98e488
Aureo Júnior <aureojr1@gmail.com>
2016-04-11 03:00:48 -0300

    templates

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,16 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/', function ($request, $response) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.phtml');
+});
+
+$app->get('/login', function ( $request,  $response) {
+    $this->logger->addInfo("Acessando a tela de login");
+    $response = $this->view->render("login.phtml");
+    return $response;
 });
